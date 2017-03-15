b3f6df0d0c263f0ccb896ef2a695779bace07489
ZoranGj <zoran.gjuroski@gmail.com>
2017-03-15 23:41:53 +0100

    Initial commit: ASP.NET MVC project setup, add initial views.

---
+++ CodeSnippets/CodeSnippets/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CodeSnippets.Controllers {
+	public class HomeController : Controller {
+		public ActionResult Index() {
+			return View();
+		}
+
+		public ActionResult Insert() {
+			return View(); 
+		}
+
+		public ActionResult Snippets() {
+			return View();
+		}
+
+		public ActionResult About() {
+			return View(); 
+		}
+	}
+}
